3d01095a3390bc7b1b77374a15d08c7e566a388f
Funcker <funcker39@gmail.com>
2022-06-01 13:30:31 +0200

    Changement saut de ligne

--- move.c
+++ move.c
@@ -3,7 +3,7 @@
 
 int canMovePiece(piece board[12][12], int dimension, int from[2], int to[2], int turnColor) {
     piece selectedPiece = board[from[0]][from[1]];
-    printf("\nVous voulez déplacer la pièce %c%c vers %c%d\n", 
+    printf("\nVous voulez déplacer la pièce %c%c vers %c%d.", 
         pieceChars[selectedPiece.type], colorChars[selectedPiece.color],
         numberToAsciiLetter(to[0]), dimension - to[1]);
 
@@ -20,15 +20,15 @@ int canChoosePiece(piece board[12][12], int dimension, int piecePos[2], int turn
 
     switch (selectedPiece.type) {
         case out:
-            printf("La case choisie est hors du plateau.\n");
+            printf("\nLa case choisie est hors du plateau.");
             return 0;
         case empty:
-            printf("Il n'y a pas de pièce sur la case choisie.\n");
+            printf("\nIl n'y a pas de pièce sur la case choisie.");
             return 0;
     }
 
     if (selectedPiece.color != turnColor) {
-        printf("La pièce choisie ne vous appartient pas.\n");
+        printf("\nLa pièce choisie ne vous appartient pas.");
         return 0;
     }
 
