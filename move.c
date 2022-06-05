77943133b8e1fbad7b01b728bdcc09747b1f1d5d
Funcker <funcker39@gmail.com>
2022-06-05 16:10:13 +0200

    Test fix compilation

--- move.c
+++ move.c
@@ -2,10 +2,10 @@
 #include "globalVar.h"
 #include "board.h"
 
-//Fonction principale permettant de verifier si une piece peut effectuer un deplacement
+// Fonction principale permettant de verifier si une piece peut effectuer un deplacement
 int canMovePiece(piece board[12][12], int dimension, int from[2], int to[2], int turnColor, int checkTest)
 {
-    piece selectedPiece = board[from[0]][from[1]];//Recuperation de la pièce
+    piece selectedPiece = board[from[0]][from[1]]; // Recuperation de la pièce
 
     int canMove = 0;
 
@@ -13,12 +13,12 @@ int canMovePiece(piece board[12][12], int dimension, int from[2], int to[2], int
         to[0] < 0 || to[0] >= dimension || to[1] < 0 || to[1] >= dimension)
         return 0;
 
-    if (board[to[0]][to[1]].type != empty && board[to[0]][to[1]].color == selectedPiece.color) //Verification de si la case destination n'est pas deja occupée par une piece de la meme couleur
+    if (board[to[0]][to[1]].type != empty && board[to[0]][to[1]].color == selectedPiece.color) // Verification de si la case destination n'est pas deja occupée par une piece de la meme couleur
     {
         return 0;
     }
 
-    switch (selectedPiece.type)//En fonction du type de piece on appelle la fonction correspondante pour verifier si un mouvement est possible.
+    switch (selectedPiece.type) // En fonction du type de piece on appelle la fonction correspondante pour verifier si un mouvement est possible.
     {
     case pawn:
         if (canMovePawn(board, from, to, turnColor))
@@ -58,10 +58,10 @@ int canMovePiece(piece board[12][12], int dimension, int from[2], int to[2], int
         break;
     }
 
-    if (canMove && checkTest) //On verifie si le roi est en echec
+    if (canMove && checkTest) // On verifie si le roi est en echec
     {
-       
-        piece tmpBoard[12][12]; //Copie du plateau dans un tableau temporaire
+
+        piece tmpBoard[12][12]; // Copie du plateau dans un tableau temporaire
 
         for (int x = 0; x < 12; x++)
         {
@@ -70,20 +70,19 @@ int canMovePiece(piece board[12][12], int dimension, int from[2], int to[2], int
                 tmpBoard[x][y] = board[x][y];
             }
         }
-       
 
-        updateBoard(tmpBoard, dimension, from, to); //On met a jour le plateau temporaire
+        updateBoard(tmpBoard, dimension, from, to); // On met a jour le plateau temporaire
 
-        return !isInCheck(tmpBoard, dimension, turnColor);//On verifie si le roi est en echec dans le plateau temporaire (si isInCheck renvoie 1 on renvoie 0 car le mouvement n'est pas possible)
+        return !isInCheck(tmpBoard, dimension, turnColor); // On verifie si le roi est en echec dans le plateau temporaire (si isInCheck renvoie 1 on renvoie 0 car le mouvement n'est pas possible)
     }
 
     return canMove;
 }
 
-//Fonction permettant de verifier si la piece peut etre choisie ou non
+// Fonction permettant de verifier si la piece peut etre choisie ou non
 int canChoosePiece(piece board[12][12], int dimension, int piecePos[2], int turnColor)
 {
-    piece selectedPiece = board[piecePos[0]][piecePos[1]];//On recupere la piece
+    piece selectedPiece = board[piecePos[0]][piecePos[1]]; // On recupere la piece
 
     switch (selectedPiece.type)
     {
@@ -109,14 +108,14 @@ void printBlockedPiece()
     // printf("\nLa case que vous voulez atteindre n'est pas accessible.");
 }
 
-//Fonction permettant de verifier si le roi est en echec
+// Fonction permettant de verifier si le roi est en echec
 int isInCheck(piece board[12][12], int dimension, int color)
 {
-    int playerKingPos[2]; //Coordonées du roi
-    int opponentPiecesPos[dimension * 2][2]; //Coordonées de toutes les pièces adverses
+    int playerKingPos[2];                    // Coordonées du roi
+    int opponentPiecesPos[dimension * 2][2]; // Coordonées de toutes les pièces adverses
     int opponentPiecesIndex = 0;
 
-    //Remplissage des tableaux initalisées ci dessus
+    // Remplissage des tableaux initalisées ci dessus
     for (int x = 0; x < dimension; x++)
     {
         for (int y = 0; y < dimension; y++)
@@ -140,7 +139,7 @@ int isInCheck(piece board[12][12], int dimension, int color)
         }
     }
 
-    //On verifie si les pièces adverses peuvent mettre le roi en echec en verifiant si elles peuvent atteindre le roi à l'aide de la fonction canMovePiece
+    // On verifie si les pièces adverses peuvent mettre le roi en echec en verifiant si elles peuvent atteindre le roi à l'aide de la fonction canMovePiece
     for (int k = 0; k < opponentPiecesIndex; k++)
     {
         if (canMovePiece(board, dimension, opponentPiecesPos[k], playerKingPos, !color, 0))
@@ -151,14 +150,14 @@ int isInCheck(piece board[12][12], int dimension, int color)
     return 0;
 }
 
-//Fonction permettant de verifier l'echec et mat
+// Fonction permettant de verifier l'echec et mat
 int isInCheckMate(piece board[12][12], int dimension, int color)
 {
-    int playerKingPos[2]; //Coordonées du roi
-    int allyPiecesPos[dimension * 2][2];//Coordonées des pièces alliées
+    int playerKingPos[2];                // Coordonées du roi
+    int allyPiecesPos[dimension * 2][2]; // Coordonées des pièces alliées
     int allyPiecesIndex = 0;
 
-    //Remplissage des tableaux initialisés ci-dessus
+    // Remplissage des tableaux initialisés ci-dessus
     for (int x = 0; x < dimension; x++)
     {
         for (int y = 0; y < dimension; y++)
@@ -184,7 +183,7 @@ int isInCheckMate(piece board[12][12], int dimension, int color)
 
     // On essaie de déplacer chaque pièce alliée dans toutes les potentiels positions en vérifiant si le déplacement est possible
     // et si le roi est toujours en échec.
-    // On return 0 (pas en échec et mat) si un des déplacements permet d'éviter l'échec sinon on return 1 (joueur en échec et mat) 
+    // On return 0 (pas en échec et mat) si un des déplacements permet d'éviter l'échec sinon on return 1 (joueur en échec et mat)
     for (int k = 0; k < allyPiecesIndex; k++)
     {
         int curPiecePos[2];
@@ -196,6 +195,7 @@ int isInCheckMate(piece board[12][12], int dimension, int color)
         switch (curPiece.type)
         {
         case pawn:
+        {
             int dir = color ? 1 : -1;
             int moves[3][2] = {{1, dir}, {0, dir}, {-1, dir}};
             for (int i = 0; i < 3; i++)
@@ -207,11 +207,14 @@ int isInCheckMate(piece board[12][12], int dimension, int color)
                 }
             }
             break;
+        }
+
         case bishop:
+        {
             for (int i = 0; i < dimension; i++)
             {
                 int moves1[4][2] = {{i, i}, {i, -i}, {-i, i}, {-i, -i}};
-                
+
                 for (int k = 0; k < 4; k++)
                 {
                     int move[2] = {curPiecePos[0] + moves1[k][0], curPiecePos[1] + moves1[k][1]};
@@ -222,7 +225,10 @@ int isInCheckMate(piece board[12][12], int dimension, int color)
                 }
             }
             break;
+        }
+
         case knight:
+        {
             int moves2[8][2] = {{2, 1}, {2, -1}, {-2, 1}, {-2, -1}, {1, 2}, {-1, 2}, {1, -2}, {-1, -2}};
             for (int i = 0; i < 8; i++)
             {
@@ -233,7 +239,10 @@ int isInCheckMate(piece board[12][12], int dimension, int color)
                 }
             }
             break;
+        }
+
         case rook:
+        {
             for (int i = 0; i < dimension; i++)
             {
                 int moves3[4][2] = {{i, 0}, {0, i}, {-i, 0}, {0, -i}};
@@ -247,7 +256,10 @@ int isInCheckMate(piece board[12][12], int dimension, int color)
                 }
             }
             break;
+        }
+
         case queen:
+        {
             for (int i = 0; i < dimension; i++)
             {
                 int moves4[8][2] = {{i, 0}, {0, i}, {-i, 0}, {0, -i}, {i, i}, {i, -i}, {-i, i}, {-i, -i}};
@@ -261,7 +273,10 @@ int isInCheckMate(piece board[12][12], int dimension, int color)
                 }
             }
             break;
+        }
+
         case king:
+        {
             int moves5[8][2] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
             for (int i = 0; i < 8; i++)
             {
@@ -272,6 +287,7 @@ int isInCheckMate(piece board[12][12], int dimension, int color)
                 }
             }
             break;
+        }
         }
     }
 
@@ -286,7 +302,7 @@ int canMovePawn(piece board[12][12], int from[2], int to[2], int color)
         {
             if (board[to[0]][to[1]].type != empty)
             {
-          
+
                 return 0;
             }
             return 1;
@@ -348,7 +364,7 @@ int canMoveRook(piece board[12][12], int from[2], int to[2])
 
             if (curPiece.type != empty)
             {
-               
+
                 return 0;
             }
         }
@@ -367,7 +383,7 @@ int canMoveRook(piece board[12][12], int from[2], int to[2])
 
             if (curPiece.type != empty)
             {
-            
+
                 return 0;
             }
         }
@@ -384,7 +400,8 @@ int canMoveKnight(int from[2], int to[2])
     int move[2] = {from[0] - to[0], from[1] - to[1]};
     for (int i = 0; i < 8; i++)
     {
-        if (move[0] == moves[i][0] && move[1] == moves[i][1]) return 1;
+        if (move[0] == moves[i][0] && move[1] == moves[i][1])
+            return 1;
     }
     return 0;
 }
